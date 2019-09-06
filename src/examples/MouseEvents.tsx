a2edff9c85e071d8e7de7fed3a1c8b6c88b0f906
Christopher Czach <ChrisCzach@ChristophersMBP.socal.rr.com>
2019-09-05 23:30:45 -0700

    starting mouseEvent examples

--- src/examples/MouseEvents.tsx
+++ src/examples/MouseEvents.tsx
@@ -1,5 +1,14 @@
-import React, { FC } from 'react'
-
+import React, { FC, useEffect } from 'react'
+import { fromEvent } from 'rxjs'
 export const MouseEvents: FC = () => {
+  useEffect(() => {
+    const mouse = fromEvent(document, 'mousemove')
+    const observer = mouse.subscribe(console.log)
+
+    mouse.subscribe(console.log)
+    return () => {
+      observer.unsubscribe()
+    }
+  }, [])
   return <div>MouseEvents</div>
 }
