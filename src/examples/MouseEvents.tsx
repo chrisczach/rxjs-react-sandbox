6077704d47d469384969cb3ff3490072a2787177
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-09-06 08:47:22 -0700

    add mouse listener

--- src/examples/MouseEvents.tsx
+++ src/examples/MouseEvents.tsx
@@ -1,14 +1,18 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { fromEvent } from 'rxjs'
+import { pluck } from 'rxjs/operators'
 export const MouseEvents: FC = () => {
+  const [mouseXY, setMouseXY] = useState()
   useEffect(() => {
     const mouse = fromEvent(document, 'mousemove')
-    const observer = mouse.subscribe(console.log)
+    const observer = mouse.subscribe(({ clientX, clientY }: any) =>
+      setMouseXY({ clientX, clientY })
+    )
 
     mouse.subscribe(console.log)
     return () => {
       observer.unsubscribe()
     }
   }, [])
-  return <div>MouseEvents</div>
+  return <div>{JSON.stringify(mouseXY)}</div>
 }
