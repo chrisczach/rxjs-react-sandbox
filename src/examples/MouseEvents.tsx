03e9c9751a1106ce856b0ae70e0d03f7dd8a5770
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-09-06 09:46:09 -0700

    add x y track

--- src/examples/MouseEvents.tsx
+++ src/examples/MouseEvents.tsx
@@ -1,18 +1,29 @@
 import React, { FC, useEffect, useState } from 'react'
 import { fromEvent } from 'rxjs'
-import { pluck } from 'rxjs/operators'
+import { takeLast } from 'rxjs/operators'
+
+import styles from './MouseEvents.module.css'
+
 export const MouseEvents: FC = () => {
-  const [mouseXY, setMouseXY] = useState()
+  const [mouseXY, setMouseXY] = useState({ x: 0, y: 0 })
   useEffect(() => {
     const mouse = fromEvent(document, 'mousemove')
-    const observer = mouse.subscribe(({ clientX, clientY }: any) =>
-      setMouseXY({ clientX, clientY })
+    const observer = mouse.subscribe(({ clientX: x, clientY: y }: any) =>
+      requestAnimationFrame(() => setMouseXY({ x, y }))
     )
 
-    mouse.subscribe(console.log)
     return () => {
       observer.unsubscribe()
     }
   }, [])
-  return <div>{JSON.stringify(mouseXY)}</div>
+  return (
+    <div className={styles.wrapper}>
+      <div style={{ left: mouseXY.x + 'px' }} className={styles.vertical} />
+      <div style={{ top: mouseXY.y + 'px' }} className={styles.horizontal} />
+      <div
+        style={{ top: mouseXY.y + 'px', left: mouseXY.x + 'px' }}
+        className={styles.crosshair}
+      />
+    </div>
+  )
 }
