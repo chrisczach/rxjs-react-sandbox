681d2a830190870a1e12f58f87657f69151b5a75
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-09-06 10:52:39 -0700

    add mouse up and down listeners

--- src/examples/MouseEvents.tsx
+++ src/examples/MouseEvents.tsx
@@ -6,18 +6,51 @@ import styles from './MouseEvents.module.css'
 
 export const MouseEvents: FC = () => {
   const [mouseXY, setMouseXY] = useState({ x: 0, y: 0 })
+  const [mouseDownXY, setMouseDownXY] = useState({ isDown: false, x: 0, y: 0 })
+  const [mouseUpXY, setMouseUpXY] = useState({ x: 0, y: 0 })
+
   useEffect(() => {
-    const mouse = fromEvent(document, 'mousemove')
-    const observer = mouse.subscribe(({ clientX: x, clientY: y }: any) =>
-      requestAnimationFrame(() => setMouseXY({ x, y }))
+    const mouseMove = fromEvent(document, 'mousemove').subscribe(
+      ({ clientX: x, clientY: y }: any) =>
+        requestAnimationFrame(() => setMouseXY({ x, y }))
     )
 
+    const mouseDown = fromEvent(document, 'mousedown').subscribe(
+      ({ clientX: x, clientY: y }: any) =>
+        requestAnimationFrame(() => setMouseDownXY({ isDown: true, x, y }))
+    )
+
+    const mouseUp = fromEvent(document, 'mouseup').subscribe(
+      ({ clientX: x, clientY: y }: any) =>
+        requestAnimationFrame(() => {
+          setMouseDownXY(prevState => ({ ...prevState, isDown: false }))
+          setMouseUpXY({ x, y })
+        })
+    )
     return () => {
-      observer.unsubscribe()
+      mouseMove.unsubscribe()
+      mouseDown.unsubscribe()
+      mouseUp.unsubscribe()
     }
   }, [])
+
   return (
     <div className={styles.wrapper}>
+      <div className={styles.stats}>{`Position ${JSON.stringify(
+        mouseXY,
+        null,
+        2
+      )}`}</div>
+      <div className={styles.stats}>{`Mouse Down Position ${JSON.stringify(
+        mouseDownXY,
+        null,
+        2
+      )}`}</div>
+      <div className={styles.stats}>{`Mouse Up Position ${JSON.stringify(
+        mouseUpXY,
+        null,
+        2
+      )}`}</div>
       <div style={{ left: mouseXY.x + 'px' }} className={styles.vertical} />
       <div style={{ top: mouseXY.y + 'px' }} className={styles.horizontal} />
       <div
