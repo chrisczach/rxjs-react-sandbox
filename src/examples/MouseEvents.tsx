dc3e4e31faf147c217e2f6c0513acaa2ceadace7
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-09-06 12:40:46 -0700

    add current square

--- src/examples/MouseEvents.tsx
+++ src/examples/MouseEvents.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect, useState } from 'react'
 import { fromEvent } from 'rxjs'
-import { takeLast } from 'rxjs/operators'
+import { debounceTime } from 'rxjs/operators'
 
 import styles from './MouseEvents.module.css'
 
@@ -8,6 +8,26 @@ export const MouseEvents: FC = () => {
   const [mouseXY, setMouseXY] = useState({ x: 0, y: 0 })
   const [mouseDownXY, setMouseDownXY] = useState({ isDown: false, x: 0, y: 0 })
   const [mouseUpXY, setMouseUpXY] = useState({ x: 0, y: 0 })
+  const [squares, setSquares]: any = useState([])
+
+  // const addSquare = ({
+  //   start,
+  //   end
+  // }: {
+  //   start: { x: number; y: number }
+  //   end: { x: number; y: number }
+  // }) => {
+  //   const x = Math.min(start.x, end.x)
+  //   const y = Math.min(start.y, end.y)
+  //   const width = Math.max(start.x, end.x) - x + 'px'
+  //   const height = Math.max(start.y, end.y) - y + 'px'
+  //   setSquares((prev: any) => [
+  //     ...prev,
+  //     <div
+  //       style={{ top: y + 'px', left: x + 'px', width, height }}
+  //       className={styles.square}></div>
+  //   ])
+  // }
 
   useEffect(() => {
     const mouseMove = fromEvent(document, 'mousemove').subscribe(
@@ -16,16 +36,18 @@ export const MouseEvents: FC = () => {
     )
 
     const mouseDown = fromEvent(document, 'mousedown').subscribe(
-      ({ clientX: x, clientY: y }: any) =>
+      ({ clientX: x, clientY: y }: any) => {
         requestAnimationFrame(() => setMouseDownXY({ isDown: true, x, y }))
+      }
     )
 
     const mouseUp = fromEvent(document, 'mouseup').subscribe(
-      ({ clientX: x, clientY: y }: any) =>
+      ({ clientX: x, clientY: y }: any) => {
         requestAnimationFrame(() => {
-          setMouseDownXY(prevState => ({ ...prevState, isDown: false }))
           setMouseUpXY({ x, y })
+          setMouseDownXY(prevState => ({ ...prevState, isDown: false }))
         })
+      }
     )
     return () => {
       mouseMove.unsubscribe()
@@ -57,6 +79,33 @@ export const MouseEvents: FC = () => {
         style={{ top: mouseXY.y + 'px', left: mouseXY.x + 'px' }}
         className={styles.crosshair}
       />
+
+      {squares}
+      <CurrentSquare start={mouseDownXY} end={mouseXY} />
     </div>
+  )
+}
+
+const CurrentSquare = ({
+  start,
+  end
+}: {
+  start: { x: number; y: number; isDown: boolean }
+  end: { x: number; y: number }
+}) => {
+  const x = Math.min(start.x, end.x)
+  const y = Math.min(start.y, end.y)
+  const width = Math.max(start.x, end.x) - x
+  const height = Math.max(start.y, end.y) - y
+  return (
+    <div
+      style={{
+        opacity: start.isDown ? 1 : 0,
+        left: x + 'px',
+        top: y + 'px',
+        width: width + 'px',
+        height: height + 'px'
+      }}
+      className={styles.currentSquare}></div>
   )
 }
