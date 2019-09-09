f3602e05df7378577daa1caec05588df5ea6e138
Christopher Czach <ChrisCzach@ChristophersMBP.socal.rr.com>
2019-09-08 21:21:57 -0700

    oops

--- src/examples/StopWatch/index.tsx
+++ src/examples/StopWatch/index.tsx
@@ -1,45 +1,78 @@
-import React, { FC, useState, useEffect, SetStateAction } from 'react'
-import { interval, animationFrameScheduler, from } from 'rxjs'
-import { tap } from 'rxjs/operators'
+import React, { FC, useState, useEffect } from 'react'
+import { interval, animationFrameScheduler, fromEvent } from 'rxjs'
+import { tap, map, withLatestFrom } from 'rxjs/operators'
 
 export const StopWatch: FC = () => {
-  const [timerState, setTimerState]: [StopWatchAction, Function] = useState(
-    stopped
-  )
-
-  const [time, setTime]: [number, Function] = useState(0)
+  const [isRunning, setRunning] = useState(false)
+  const [currentTime, setCurrentTime]: [number, Function] = useState(0)
+  const [startTime, setStartTime]: [number, Function] = useState(0)
   const [laps, setLaps]: [Array<any>, Function] = useState([])
+  const [pausedTime, setPausedTime] = useState(0)
+  const totalTime: any = isRunning ? currentTime - startTime + pausedTime : 0
 
-  const updateTime = (num: any) => setTime(num / 100)
-  const addLap = () => setLaps((prev: any) => [...prev, time])
+  const updateTime = (time: any) => setCurrentTime(time)
+  const toSeconds = (num: any) => (num / 100).toFixed(2)
+  const addLap = () => console.log('add lap ' + isRunning)
+  // setLaps((prev: any) => [...prev, currentTime - startTime])
+  const startStopAction = (time: number) => {
+    setRunning((prev: boolean) => {
+      if (prev) {
+        setPausedTime((_: any) => currentTime - startTime + pausedTime)
+        return !prev
+      } else {
+        setStartTime(time)
+        return true
+      }
+    })
+  }
   const resetLaps = () => setLaps([])
-  const updateTimerState = ({ target: { value } }: any) => {
-    if (value === reset) resetLaps()
-    setTimerState(value)
+  const resetAction = () => {
+    setStartTime(0)
+    resetLaps()
+    setRunning(false)
+  }
+  const updateState = ({
+    action,
+    time
+  }: {
+    action: StopWatchAction
+    time: number
+  }) => {
+    if (action === startStop) {
+      startStopAction(time)
+    } else if (action === lap) {
+      addLap()
+    } else if (action === reset) {
+      resetAction()
+    }
   }
 
   useEffect(() => {
-    const timer$ = interval(10, animationFrameScheduler)
-    const timerObserver = timer$.pipe(tap(updateTime)).subscribe()
+    console.log('effect rendered')
+    const time$ = interval(10, animationFrameScheduler).pipe(
+      map(toSeconds),
+      tap(updateTime)
+    )
+    const timerControl$ = fromEvent(document, 'click').pipe(
+      withLatestFrom(time$),
+      map(toClickValue),
+      tap(updateState)
+    )
+
+    const timerObserver = timerControl$.subscribe()
     return () => {
       timerObserver.unsubscribe()
     }
   }, [])
   return (
     <>
-      <div>Timer State: {timerState}</div>
-      <div>Time: {time}</div>
-      <button
-        onClick={updateTimerState}
-        value={timerState === stopped ? running : stopped}>
-        Start/Stop
-      </button>
-      <button onClick={addLap} value={running}>
-        Lap
-      </button>
-      <button onClick={updateTimerState} value={reset}>
-        reset
-      </button>
+      <div>Running: {isRunning ? 'Running' : 'Stopped'}</div>
+      <div>Start Time: {startTime}</div>
+      <div>Current Time: {currentTime}</div>
+      <div>Total Time: {totalTime}</div>
+      <button value={startStop}>Start/Stop</button>
+      <button value={lap}>Lap</button>
+      <button value={reset}>reset</button>
       {laps.map(timesToLaps)}
     </>
   )
@@ -54,13 +87,20 @@ const timesToLaps = (time: number, i: number, a: Array<number>) => (
   </div>
 )
 
-const [stopped, running, reset]: Array<StopWatchAction> = [
-  'stopped',
-  'running',
+const toClickValue = ([
+  {
+    target: { value: action }
+  },
+  time
+]: any) => ({ action, time })
+
+const [startStop, lap, reset]: Array<StopWatchAction> = [
+  'startStop',
+  'lap',
   'reset'
 ]
 
-type StopWatchAction = 'stopped' | 'running' | 'reset'
+type StopWatchAction = 'startStop' | 'lap' | 'reset'
 
 export default {
   StopWatch
