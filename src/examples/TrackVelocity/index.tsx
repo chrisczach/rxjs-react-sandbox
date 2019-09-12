909122935bb2a17b5f968ce4c643562d65145e38
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-09-12 08:40:11 -0700

    need to fix click to pause

--- src/examples/TrackVelocity/index.tsx
+++ src/examples/TrackVelocity/index.tsx
@@ -1,8 +1,15 @@
 import React, { FC, useRef, MutableRefObject } from 'react'
 import styles from './index.module.css'
 import { useEventCallback } from 'rxjs-hooks'
-import { map, switchMap, scan, tap } from 'rxjs/operators'
-import { of, from, concat, fromEvent, NEVER, Observable } from 'rxjs'
+import {
+  map,
+  switchMap,
+  scan,
+  tap,
+  switchMapTo,
+  mergeMap
+} from 'rxjs/operators'
+import { of, from, concat, fromEvent, NEVER, Observable, interval } from 'rxjs'
 
 export const TrackVelocity: FC = () => {
   const trackableRef: any = useRef(null)
@@ -11,8 +18,9 @@ export const TrackVelocity: FC = () => {
       const starter$ = event$.pipe(
         startPause,
         listenToMouseMoves,
-        mapToTranslateXY,
-        logState(trackableRef)
+        mapToTranslateXY(trackableRef),
+        translateElementXY,
+        logState
       )
 
       return starter$
@@ -35,7 +43,27 @@ const listenToMouseMoves = switchMap(
     started ? fromEvent(document, 'mousemove') : NEVER
 )
 
-const mapToTranslateXY = map((status: any): any => status)
+const mapToTranslateXY = (ref: any) =>
+  map(({ clientX, clientY }: any): any => {
+    const elementX = ref.current.offsetLeft
+    const elementY = ref.current.offsetTop
+    const offsetX = clientX - elementX
+    const offsetY = clientY - elementY
+    return {
+      clientX,
+      clientY,
+      elementX,
+      elementY,
+      offsetX,
+      offsetY
+    }
+  })
+
+const translateElementXY = switchMap(({ offsetX, offsetY, ...rest }: any) =>
+  offsetX !== 0 || offsetY !== 0
+    ? interval(100).pipe(map((_: any) => ({ ...rest, offsetX, offsetY })))
+    : NEVER
+)
 
 // const translateTarget = ({ current: { offSetTop, offSetLeft } }) =>
 //   tap(({ status: { clientY, clientX } }: any) => {
@@ -43,16 +71,8 @@ const mapToTranslateXY = map((status: any): any => status)
 //     offSetLeft = offSetTop - (clientX - offSetTop) / 2
 //   })
 
-const logState = (ref: any) =>
-  tap((status: any) => {
-    console.log(
-      'chaserTop: ' +
-        ref.current.offsetTop +
-        ' chaserLeft:' +
-        ref.current.offsetLeft
-    )
-    console.log('mouseTop: ' + status.clientY + ' mouseLeft:' + status.clientX)
-  })
+const logState = tap((state: any) => console.log(JSON.stringify(state)))
+
 export default {
   TrackVelocity
 }
