9132c181ca4257a5761596df80306ede1756700a
Christopher Czach <ChrisCzach@ChristophersMBP.socal.rr.com>
2019-09-11 20:34:11 -0700

    adding mouse chaser

--- src/examples/TrackVelocity/index.tsx
+++ src/examples/TrackVelocity/index.tsx
@@ -1,8 +1,43 @@
-import React, { FC } from 'react'
+import React, { FC, useRef } from 'react'
+import styles from './index.module.css'
+import { useEventCallback } from 'rxjs-hooks'
+import { map, switchMap, scan, tap } from 'rxjs/operators'
+import { of, from, concat, fromEvent } from 'rxjs'
 export const TrackVelocity: FC = () => {
-  
-  return <div>TrackVelocity</div>
+  const trackableRef = useRef(null)
+  const [clickCallback, [status]] = useEventCallback(
+    event$ => {
+      const starter$ = event$.pipe(scan(togglePause, false))
+      return starter$.pipe(
+        switchMap(started =>
+          started ? fromEvent(document, 'mousemove') : from(['stopped'])
+        ),
+        map((status: any): any => [status, trackableRef]),
+        tap((state: any) => {
+          console.log(
+            'chaserTop: ' +
+              state[1].current.offsetTop +
+              ' chaserLeft:' +
+              state[1].current.offsetLeft
+          )
+          console.log(
+            'mouseTop: ' + state[0].clientY + ' mouseLeft:' + state[0].clientX
+          )
+        })
+      )
+    },
+    ['stopped', trackableRef]
+  )
+  return (
+    <>
+      <div onClick={clickCallback} className={styles.wrapper}>
+        <div ref={trackableRef} className={styles.chaser} />
+      </div>
+    </>
+  )
 }
+
+const togglePause = (acc: boolean): boolean => !acc
 
 export default {
   TrackVelocity
