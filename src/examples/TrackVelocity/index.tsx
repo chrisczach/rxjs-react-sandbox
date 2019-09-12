a83e16ab4cde588d0b35ba284a5298de9a63b682
Christopher Czach <ChrisCzach@ChristophersMBP.socal.rr.com>
2019-09-11 21:26:18 -0700

    refactor

--- src/examples/TrackVelocity/index.tsx
+++ src/examples/TrackVelocity/index.tsx
@@ -1,32 +1,23 @@
-import React, { FC, useRef } from 'react'
+import React, { FC, useRef, MutableRefObject } from 'react'
 import styles from './index.module.css'
 import { useEventCallback } from 'rxjs-hooks'
 import { map, switchMap, scan, tap } from 'rxjs/operators'
-import { of, from, concat, fromEvent } from 'rxjs'
+import { of, from, concat, fromEvent, NEVER, Observable } from 'rxjs'
+
 export const TrackVelocity: FC = () => {
-  const trackableRef = useRef(null)
-  const [clickCallback, [status]] = useEventCallback(
+  const trackableRef: any = useRef(null)
+  const [clickCallback, status]: any = useEventCallback(
     event$ => {
-      const starter$ = event$.pipe(scan(togglePause, false))
-      return starter$.pipe(
-        switchMap(started =>
-          started ? fromEvent(document, 'mousemove') : from(['stopped'])
-        ),
-        map((status: any): any => [status, trackableRef]),
-        tap((state: any) => {
-          console.log(
-            'chaserTop: ' +
-              state[1].current.offsetTop +
-              ' chaserLeft:' +
-              state[1].current.offsetLeft
-          )
-          console.log(
-            'mouseTop: ' + state[0].clientY + ' mouseLeft:' + state[0].clientX
-          )
-        })
+      const starter$ = event$.pipe(
+        startPause,
+        listenToMouseMoves,
+        mapToTranslateXY,
+        logState(trackableRef)
       )
+
+      return starter$
     },
-    ['stopped', trackableRef]
+    [trackableRef]
   )
   return (
     <>
@@ -37,8 +28,31 @@ export const TrackVelocity: FC = () => {
   )
 }
 
-const togglePause = (acc: boolean): boolean => !acc
+const startPause = scan((acc: boolean): boolean => !acc, false)
 
+const listenToMouseMoves = switchMap(
+  (started: boolean): Observable<Event> =>
+    started ? fromEvent(document, 'mousemove') : NEVER
+)
+
+const mapToTranslateXY = map((status: any): any => status)
+
+// const translateTarget = ({ current: { offSetTop, offSetLeft } }) =>
+//   tap(({ status: { clientY, clientX } }: any) => {
+//     offSetTop = offSetTop - (clientY - offSetTop) / 2
+//     offSetLeft = offSetTop - (clientX - offSetTop) / 2
+//   })
+
+const logState = (ref: any) =>
+  tap((status: any) => {
+    console.log(
+      'chaserTop: ' +
+        ref.current.offsetTop +
+        ' chaserLeft:' +
+        ref.current.offsetLeft
+    )
+    console.log('mouseTop: ' + status.clientY + ' mouseLeft:' + status.clientX)
+  })
 export default {
   TrackVelocity
 }
